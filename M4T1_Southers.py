0742bda3ab7d35450c66b42249711d25aa75faf2
southerc <33227053+southerc@users.noreply.github.com>
2017-11-13 11:37:43 -0500

    Tutorial

---
+++ M4T1_Southers.py
@@ -0,0 +1,10 @@
+#CTI-110
+#M4T1 - Saes Predction
+#Christian Southers
+#11/13/2017
+totalSales = float(input('Enter the projected sales:  '))
+
+
+profit = totalSales * 0.23
+
+print ('The profit is $', format(profit,  ',.2f'))
